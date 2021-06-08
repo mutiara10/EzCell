c5c3a15c46364b8c0dffecf7cb3a2c7b0f553543
fearhunt <firhanmahdi@gmail.com>
2021-06-08 20:21:45 +0700

    add camera (WIP)

--- app/src/main/java/com/example/ezcell/fm/CameraFragment.kt
+++ app/src/main/java/com/example/ezcell/fm/CameraFragment.kt
@@ -1,21 +1,33 @@
 package com.example.ezcell.fm
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ezcell.CameraActivity
 import com.example.ezcell.R
+import com.example.ezcell.databinding.FragmentCameraBinding
 
 
 class CameraFragment : Fragment() {
+    private lateinit var binding: FragmentCameraBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        binding = FragmentCameraBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.btnCamera.setOnClickListener {
+            val intent = Intent(activity, CameraActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
